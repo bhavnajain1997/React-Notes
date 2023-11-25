6bac0add5796a96b26b0ef97ff8d4167bd323806
Bhavna Jain <jbhavna732@gmail.com>
2023-11-25 23:00:17 +0530

    commit

---
+++ Episode-2 Igniting/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const parent = React.createElement(
+    "div",{id:"parent"}, 
+    React.createElement("div",{id:"child"},
+    [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]
+    )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
