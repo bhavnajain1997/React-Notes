04a50e3b7a2d6d64b64e1fa2eee5519a2c4029ef
Bhavna Jain <jbhavna732@gmail.com>
2024-02-20 16:21:59 +0530

    second commit

--- Episode-2 Igniting/App.js
+++ Episode-2 Igniting/App.js
@@ -1,10 +1,49 @@
 import React from "react";
-import ReactDOM from "react-dom";
-const parent = React.createElement(
-    "div",{id:"parent"}, 
-    React.createElement("div",{id:"child"},
-    [React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]
-    )
+import ReactDOM from "react-dom/client";
+
+// React.createElement => ReactElement-JS Object => when we render this object it will be converted in Html Element => HTML Element(render)
+// Simple way without JSX
+// const heading = React.createElement("h1",{id:"heading"},"Namaste React!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+//with JSX : - JSX is not HTML in Javascript.JSX is a HTML-like or XML-like syntax.
+//JSX - (transpiled before it reaches the JS Engine.) -> PARCEL -> BABEL(Babel is converted this code quickly that react will understand).
+
+
+// JSX => React.createElement
+// const heading = (
+// <h1 className = "heading" tabIndex="5">
+//     Namaste React using React!
+//     </h1>);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// React Functional Component
+// Arrow function
+// const Title =() => (
+//     <h1 className="head" tabIndex= "5">
+//         Namaste React using JSX
+//     </h1>  
+// );
+
+// Normal Function
+const number = 1000;
+const Title = function () {
+    return (
+      <h1 className="head" tabIndex= "5">
+        Namaste React using JSX.</h1>
+    );
+};
+const HeadingComponent = () => (
+    <div id= "container">
+        <Title />
+        <h2>{number}</h2> 
+         <h1>Namaste React Functional Component.</h1>
+    </div>
+    
+
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent/>)
+     
---
+++ Episode-2 Igniting/README.md
@@ -0,0 +1,22 @@
+// Namaste React
+
+
+// Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundles all the files 
+- Compress our file
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic behind the scenes
+- Good Error Handling - It gives you better error suggestions
+- Parcel also give you way to host your app on https
+- Tree shaking - remove unused code for you
+- Differnet dev and Production bundles
+
