919522d7b2e2058427723fb09ba23bc914559b0f
Bhavna Jain <jbhavna732@gmail.com>
2024-02-22 01:18:57 +0530

    Components

--- Episode-3 laying-the-foundation/App.js
+++ Episode-3 laying-the-foundation/App.js
@@ -5,7 +5,25 @@ import ReactDOM from "react-dom"
 const heading = React.createElement("h1",{id:"heading"},"Namaste React ");
 
 //With JSX
-
-const jsxHeading = <h1 id="heading">Namaste React Using JSX</h1>
+//React Element
+const jsxHeading = (
+<h1 id="heading" className="head" tabIndex={5}>
+    Namaste React Using JSX
+    </h1>
+    )
+;
+//////////////
+const Title = () => (
+    <h1 className="head">Hello world</h1>
+);
+// React Functional Component:-
+// Component Composition
+const HeadingComponent = () => (
+    <div className="container">
+      {Title()}  
+     <Title/>
+    <h1>Namaste React Functional Component</h1>
+    </div>
+)
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading)
+root.render(<HeadingComponent/>)
